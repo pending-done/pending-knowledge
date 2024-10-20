af599b0b01f577f598798b3adc5d45e7b2a3797b
LeeJY97 <ijoonyeol97@gmail.com>
2024-10-21 03:04:13 +0900

    빌드 에러 제거, 및 about 관련 내용 제거

--- src/components/about/project-modal-content.tsx
+++
@@ -1,5 +0,0 @@
-import { DialogContent } from '@radix-ui/react-dialog';
-
-export default function ProjectModalContent({ children }: { children: React.ReactNode }) {
-  return <DialogContent>{children}</DialogContent>;
-}
--- src/config/const.ts
+++ src/config/const.ts
@@ -1,4 +1,4 @@
-export const baseDomain = 'https://localhost:3000';
+export const baseDomain = process.env.NEXT_PUBLIC_BASE_DOMAIN || '';
 
 export const blogName = '프론트 주니어 개발 블로그';
 export const blogDesc = '개발에 대해 알아가는 것들을 공유합니다.';
