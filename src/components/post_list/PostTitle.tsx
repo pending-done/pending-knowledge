be55577a3089807699b120a6861f07e66832712d
LeeJY97 <ijoonyeol97@gmail.com>
2024-12-19 18:24:13 +0900

    [fix] test

--- src/components/post_list/PostTitle.tsx
+++ src/components/post_list/PostTitle.tsx
@@ -13,7 +13,7 @@ const PostTitle = ({ post }: PostTitleProps) => {
     // <Link href={`/blog/${post.categoryPath}/${post.url}`} className='px-3'>
     <Link href={`${post.url}`} className='px-3'>
       <p className='text-gray-400 hover:text-black hover:underline'>
-        {post.order}. {post.title} : {post.url}
+        {post.order}. {post.title}
       </p>
     </Link>
   );
